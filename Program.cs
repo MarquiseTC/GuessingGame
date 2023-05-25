7e4776733ad07df835e18a801522a9bd1a2e1981
Marquise Campbell <marquisetyrrell@gmail.com>
2023-05-25 15:08:57 -0400

    all phases

--- Program.cs
+++ Program.cs
@@ -2,15 +2,41 @@
 Console.WriteLine("Guess the secret number!");
 Console.WriteLine("Take a guess.");
 
+Console.WriteLine("Choose a difficulty level:");
+Console.WriteLine("1. Easy");
+Console.WriteLine("2. Medium");
+Console.WriteLine("3. Hard");
+Console.WriteLine("4. Cheater");
 
+int choice = Convert.ToInt32(Console.ReadLine());
 
+switch (choice) {
+    case 1:
+    choice = 8;
+    Console.WriteLine("You have chosen easy mode." + " You have 8 guesses");
+    break;
 
+    case 2:
+    choice = 6;
+    Console.WriteLine("You have chosen medium mode." + " You have 6 guesses");
+    break;
 
-int secret = 42;
+    case 3:
+    choice = 4;
+    Console.WriteLine("You have chosen hard mode." + " You have 4 guesses");
+    break;
+
+    case 4:
+    choice = int.MaxValue;
+    Console.WriteLine("You have chosen cheater mode." + " Guess until correct");
+    break;
+}
+
+int secret = new Random().Next(1,100);
 
 
 int guessCount = 0;
-while( guessCount < 4)
+while( guessCount < choice)
 {
 guessCount++;
     int guess = Convert.ToInt32(Console.ReadLine());
@@ -19,13 +45,22 @@ Console.WriteLine("Wow!!! You guessed my secret!");
 break;
 }
 
-else if (guessCount < 4)
+else if (guessCount < choice && secret < guess)
 {
-Console.WriteLine("Try again next time :(");
+Console.WriteLine("Try again. Too high :("+ "Total Guesses" + (guessCount));
+}
+
+else if (guessCount < choice && secret > guess)
+{
+Console.WriteLine("Try again. Too low :("+ "Total Guesses" + (guessCount));
 }
 
 else {
-    Console.WriteLine("You ran out of guesses :(");
+    Console.WriteLine(" No more guesses." + 
+
+    "The secret number was" + ""
+    +
+    secret);
 }
 
 }
