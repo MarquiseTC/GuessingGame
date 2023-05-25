cfd0a03efda3c2fb3843d16ba6774a1074d59b21
Marquise Campbell <marquisetyrrell@gmail.com>
2023-05-25 11:06:19 -0400

    phase3

--- Program.cs
+++ Program.cs
@@ -1,7 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Guess the secret number!");
-Console.WriteLine("Choose your number.");
+Console.WriteLine("Take a guess.");
 
-int guess = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Your guess is: "  + guess);
+
+
+
+int secret = 42;
+
+
+int guessCount = 0;
+while( guessCount < 4)
+{
+guessCount++;
+    int guess = Convert.ToInt32(Console.ReadLine());
+if (secret == guess){
+Console.WriteLine("Wow!!! You guessed my secret!");
+break;
+}
+
+else if (guessCount < 4)
+{
+Console.WriteLine("Try again next time :(");
+}
+
+else {
+    Console.WriteLine("You ran out of guesses :(");
+}
+
+}
