7d98a3d573356a9c65919cc18fb569f9c6eb188b
Marquise Campbell <marquisetyrrell@gmail.com>
2023-05-25 10:10:21 -0400

    Phase1

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Guess the secret number!");
+Console.WriteLine("Choose your number.");
+
+int guess = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Your guess is: "  + guess);
